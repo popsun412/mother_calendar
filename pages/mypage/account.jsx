e37e9e8a49f8bf130ae363dcbdb32fb4860e613b
hannaclairekim <hannaclairekim@gmail.com>
2022-04-26 15:30:16 +0900

    계정관리 캘린더 공개전환 팝업 추가

--- pages/mypage/account.jsx
+++ pages/mypage/account.jsx
@@ -1,11 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { Checkbox } from '@material-ui/core';
+import { Box, Checkbox, Drawer } from '@material-ui/core';
+import { GlobalStyles } from '@mui/material';
+
+
 
 const Account = () => {
 
     const [checked1, setChecked1] = useState(true);
     const [checked2, setChecked2] = useState(true);
     const [checked3, setChecked3] = useState(true);
+
+    const [state, setState] = useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const onClick = (anchor, open, filter) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
 
     const handleChange = (e) => {
         if (e.target.value === '1') {
@@ -22,9 +40,33 @@ const Account = () => {
             setChecked1(true) : '';
     }, [])
 
+    const list = (anchor) => (
+        <Box
+        sx={{ height: 230 }}
+        role="presentation"
+        onKeyDown={onClick(anchor, false, false)}
+        >
+            <GlobalStyles 
+                styles={{
+                    '.MuiPaper-root': {
+                        borderRadius: '15px 15px 0 0'
+                    }
+                }}
+            />
+            <div className='mx-5 my-6'>
+                <div className='text-right text-xs mb-6' onClick={onClick('bottom', false, false)}>닫기</div>
+                <div>
+                    <div className='text-base mb-5'>캘린더 공개 전환하시겠어요?</div>
+                    <div className='text-sm textGray4 mb-8'>누구나 회원님의 캘린더 및 보관함을 볼 수 있습니다.</div>
+                    <div className='w-full h-12 bg5 leading-12 text-center text-white rounded-md'>캘린더 공개로 전환</div>
+                </div>
+            </div>
+        </Box>
+    );
+
     return (
         <div>
-            <header className='sticky top-0 left-0 right-0 visible opacity-100 bg-white z-100' style={{marginBottom: '-50px'}}>
+            <header className='sticky top-0 left-0 bottom-0 visible opacity-100 bg-white z-100' style={{marginBottom: '-50px'}}>
                 <div className='my-auto mx-auto py-0 px-4 relative flex items-center w-full bg-white border-b border-solid' style={{height: '50px'}}>
                     <div className='flex-1 flex items-center'>
                         <div>
@@ -46,7 +88,7 @@ const Account = () => {
                     </div>
                     <div className='flex mx-5 my-4 justify-between' style={{height: '50px'}}>
                         <div style={{fontSize: '15px', lineHeight: '50px'}}>캘린더 비공개</div>
-                        <div className='text-sm textOrange5' style={{lineHeight: '50px'}}>비공개</div>
+                        <div className='text-sm textOrange5' style={{lineHeight: '50px'}} onClick={onClick('bottom', true)}>비공개</div>
                     </div>
                     <div className='mx-5 my-4 justify-between'>
                         <div style={{fontSize: '15px', lineHeight: '50px'}}>마케팅 정보 수신 동의</div>
@@ -72,6 +114,13 @@ const Account = () => {
                         <div>|</div>
                         <div>회원탈퇴</div>
                     </div>
+                    <Drawer
+                        anchor='bottom'
+                        open={state['bottom']}
+                        onClose={onClick('bottom', false)}
+                    >
+                        {list('bottom')}
+                    </Drawer>
                 </section>
             </main>
         </div>
