c251315531f703103637962305d7cef83a324e50
hannaclairekim <hannaclairekim@gmail.com>
2022-05-02 17:20:25 +0900

    보관함_교구장 init.

---
+++ pages/addtool.jsx
@@ -0,0 +1,206 @@
+import { Global } from '@emotion/react';
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
+import React, { useState } from 'react';
+
+const AddTool = () => {
+
+    const [booktitle, setBooktitle] = useState('');
+
+    const [status, setStatus] = useState(() => ['inbox']);
+
+    const [star, setStar] = useState({
+        1: false,
+        2: false,
+        3: false,
+        4: false,
+        5: false
+    })
+
+    const titleChange = (e) => {
+        setBooktitle(e.target.value);
+    }
+
+    const handleStatus = (e) => {
+        const val = e.target.offsetParent.value;
+        setStatus(val);
+        console.log(status);
+    }
+
+    const areaChange = (index) => {
+
+    }
+
+    const starChange = (index) => {
+        setStar([e.target.offsetParent.value]);
+    }
+
+    return (
+        <div>
+            <header className='sticky top-0 left-0 right-0 visible opacity-100 bg-white z-100' style={{marginBottom: '-50px'}}>
+                <div className='my-auto mx-auto py-0 px-4 relative flex items-center w-full bg-white' style={{height: '50px'}}>
+                    <div className='flex-1 flex items-center'>
+                        <div onClick={() => {window.history.back();}}>
+                            <img src='/images/ic_back.png' />
+                        </div>
+                        <div className='my-0 mx-auto text-base font-medium' style={{letterSpacing: '-0.3px'}}>교구 등록</div>
+                        <div className='flex textGray4' style={{fontSize: '15px'}}>완료</div>
+                    </div>
+                </div>
+            </header>
+            <main style={{marginTop: '50px'}}>
+                <section className='pt-5 mx-5 my-6'>
+                    <div className='mb-6'>
+                        <div className='rounded-md my-0 mx-auto relative' style={{width: '120px', height: '120px', backgroundColor: '#f2f2f2'}}>
+                            <img src='/images/ic_camera.png' className='absolute top-10 left-10'/>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <input type='text' placeholder='교구 이름을 입력해주세요.' value={booktitle} onChange={titleChange}
+                                className='block w-full h-10 px-5 box-border border border-solid border-color4 rounded-md text-sm textGray4'/>
+                        </div>
+                    </div>
+                </section>
+                <section className='mx-5 mt-9 mb-6'>
+                    <div className='text-sm textGray2 font-medium'>상태</div>
+                    <div style={{marginTop: '18px'}}>
+                        <Global
+                            styles={{
+                                'MuiToggleButtonGroup-root': {
+                                    borderRadius: '6px'
+                                },
+                                '.MuiToggleButton-root.Mui-selected': {
+                                    backgroundColor: '#fff!important',
+                                    color: '#FF6035',
+                                    borderColor: '#FF6035',
+                                    borderRadius: '6px'
+                                },
+                            }}
+                        />
+                        <ToggleButtonGroup 
+                            value={status}
+                            onChange={handleStatus}
+                            aria-label="status" className='w-full'>
+                            <ToggleButton value="purchase" aria-label="purchase" className='w-full'>구매예정</ToggleButton>
+                            <ToggleButton value='inbox' arai-label='inbox' className='w-full'>보유중</ToggleButton>
+                            <ToggleButton value='sell' arai-label='sell' className='w-full'>판매완료</ToggleButton>
+                        </ToggleButtonGroup>
+                    </div>
+                </section>
+                <section className='mx-5 my-6'>
+                    <div className='text-sm textGray2 font-medium'>분야</div>
+                    <div className='mt-6'>
+                        <div className='grid grid-cols-4 gap-3'>
+                            <div className='flex text-sm py-2 px-2 border border-solid rounded textGray4 border-gray3 justify-center'>
+                                <img src='/images/category1.png' className='mr-1 grayscale' style={{width: '17px', height: '17px'}}/>    
+                                국어
+                            </div>
+                            <div className='flex text-sm py-2 px-2 border border-solid rounded textGray4 border-gray3 justify-center'>
+                                <img src='/images/category2.png' className='mr-1 grayscale' style={{width: '17px', height: '17px'}}/>    
+                                영어
+                            </div>
+                            <div className='flex text-sm py-2 px-2 border border-solid rounded textGray4 border-gray3 justify-center'>
+                                <img src='/images/category3.png' className='mr-1 grayscale' style={{width: '17px', height: '17px'}}/>    
+                                수학
+                            </div>
+                            <div className='flex text-sm py-2 px-2 border border-solid rounded textGray4 border-gray3 justify-center'>
+                                <img src='/images/category4.png' className='mr-1 grayscale' style={{width: '17px', height: '17px'}}/>    
+                                과학
+                            </div>
+                            <div className='flex text-sm py-2 px-2 border border-solid rounded textGray4 border-gray3 justify-center'>
+                                <img src='/images/category5.png' className='mr-1 grayscale' style={{width: '17px', height: '17px'}}/>    
+                                사회
+                            </div>
+                            <div className='flex text-sm py-2 px-2 border border-solid rounded textGray4 border-gray3 justify-center'>
+                                <img src='/images/category6.png' className='mr-1 grayscale' style={{width: '17px', height: '17px'}}/>    
+                                미술
+                            </div>
+                            <div className='flex text-sm py-2 px-2 border border-solid rounded textGray4 border-gray3 justify-center'>
+                                <img src='/images/category7.png' className='mr-1 grayscale' style={{width: '17px', height: '17px'}}/>    
+                                음악
+                            </div>
+                            <div className='flex text-sm py-2 px-2 border border-solid rounded textGray4 border-gray3 justify-center'>
+                                <img src='/images/category8.png' className='mr-1 grayscale' style={{width: '17px', height: '17px'}}/>    
+                                체육
+                            </div>
+                            <div className='flex text-sm py-2 px-2 border border-solid rounded textGray4 border-gray3 justify-center'>
+                                <img src='/images/category9.png' className='mr-1 grayscale' style={{width: '17px', height: '17px'}}/>    
+                                놀이
+                            </div>
+                            <div className='flex text-sm py-2 px-2 border border-solid rounded textGray4 border-gray3 justify-center'>
+                                <img src='/images/category11.png' className='mr-1 grayscale' style={{width: '17px', height: '17px'}}/>    
+                                기타
+                            </div>
+                            <div className='flex text-sm py-2 px-2 border border-solid rounded textGray4 border-gray3 justify-center'>
+                                <img src='/images/category12.png' className='mr-1 grayscale' style={{width: '17px', height: '17px'}}/>    
+                                부모
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className='mx-5 my-6'>
+                    <div className='text-sm textGray2 font-medium'>영역</div>
+                    <div className='mt-5 flex flex-wrap'>
+                        <label className='block relative mr-3'>
+                            <input type='checkbox' className='opacity-0 absolute top-0 left-0' onChange={areaChange}/>
+                            <span className='block text-sm textGray4 px-3 py-1.5 border border-solid border-gray3 rounded-sm'>대전집</span>
+                        </label>
+                        <label className='block relative mr-3'>
+                            <input type='checkbox' className='opacity-0 absolute top-0 left-0'/>
+                            <span className='block text-sm textGray4 px-3 py-1.5 border border-solid border-gray3 rounded-sm'>소전집</span>
+                        </label>
+                        <label className='block relative mr-3'>
+                            <input type='checkbox' className='opacity-0 absolute top-0 left-0'/>
+                            <span className='block text-sm textGray4 px-3 py-1.5 border border-solid border-gray3 rounded-sm'>단행본</span>
+                        </label>
+                        <label className='block relative mr-3'>
+                            <input type='checkbox' className='opacity-0 absolute top-0 left-0'/>
+                            <span className='block text-sm textGray4 px-3 py-1.5 border border-solid border-gray3 rounded-sm'>기타</span>
+                        </label>
+                    </div>
+                </section>
+                {
+                    status === 'purchase' ?
+                        <section className='mx-5 my-6'>
+                            <div className='text-sm textGray2 font-medium'>구매시기 <span className='textGray4'>(선택)</span></div>
+                            <div className='mt-5'>
+                                <select className='mr-6 p-1.5 text-sm border border-solid border-gray3 rounded-md bg-white'>
+                                    <option>2022년 10월</option>
+                                </select>
+                            </div>
+                        </section> : ''
+                }
+                {
+                    status === 'purchase' ?
+                        <section className='mx-5 my-6'>
+                            <div className='text-sm textGray2 font-medium'>만족도 <span className='textGray4'>(선택)</span></div>
+                            <div className='flex mt-3'>
+                                <label className='block relative'>
+                                    <input type='checkbox' className='opacity-0 absolute top-0 left-0' onChange={() => {starChange(1)}}/>
+                                    <img src={`/images/ic_star_${star[1] ? 'color@2x' : 'grey@2x'}.png`} />
+                                </label>
+                                <label className='block relative'>
+                                    <input type='checkbox' className='opacity-0 absolute top-0 left-0' onChange={() => {starChange(2)}}/>
+                                    <img src='/images/ic_star_grey@2x.png' />
+                                </label>
+                                <label className='block relative'>
+                                    <input type='checkbox' className='opacity-0 absolute top-0 left-0' onChange={() => {starChange(3)}}/>
+                                    <img src='/images/ic_star_grey@2x.png' />
+                                </label>
+                                <label className='block relative'>
+                                    <input type='checkbox' className='opacity-0 absolute top-0 left-0' onChange={() => {starChange(4)}}/>
+                                    <img src='/images/ic_star_grey@2x.png' />
+                                </label>
+                                <label className='block relative'>
+                                    <input type='checkbox' className='opacity-0 absolute top-0 left-0' onChange={() => {starChange(5)}}/>
+                                    <img src='/images/ic_star_grey@2x.png' />
+                                </label>
+                            </div>
+                        </section> : ''
+                }
+            </main>
+        </div>
+    )
+}
+
+export default AddTool;
---
+++ pages/edutool.jsx
@@ -0,0 +1,292 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Box, Drawer } from '@material-ui/core';
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
+import { Range, getTrackBackground } from 'react-range';
+
+import BookshelfInstock from '../components/bookshelf/bookshelf_instock';
+import BookshelfPurchase from '../components/bookshelf/bookshelf_purchase';
+import BookshelfSell from '../components/bookshelf/bookshlef_sell';
+import { Global } from '@emotion/react';
+
+const EduTool = () => {
+
+    const [activeTab, setActiveTab] = useState(1);
+
+    const tabClick = (index) => {
+        setActiveTab(index);
+    }
+
+    const [state, setState] = useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const onClick = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const [field, setField] = useState({});
+    const [area, setArea] = useState({});
+
+    const returnField = {};
+    const returnArea = {};
+
+    useEffect(() => {
+        fields.forEach((item, index) => {
+            field[index] = false;
+            returnField[index] = false;
+        })
+
+        areas.forEach((item, index) => {
+            area[index] = false;
+            returnArea[index] = false;
+        })
+
+    }, [])
+
+    const fieldClick = (e) => {
+        setField({ ...field, [e.target.value]: e.target.checked});
+    }
+
+    const areaClick = (e) => {
+        setArea({ ...area, [e.target.value]: e.target.checked});
+    }
+
+    const clickReturn = () => {
+        setField(returnField);
+        setArea(returnArea);
+    }
+
+    const clickApply = () => {
+        onClick(anchor, false);
+    }
+
+    const [aligns, setAligns] = useState(() => []);
+
+    const handleAligns = (e) => {
+        setAligns([e.target.offsetParent.value]);
+        console.log(aligns[0]);
+    };
+
+    const fields = [
+        {
+            id: 1,
+            label: '국어'
+        },
+        {
+            id: 2,
+            label: '영어'
+        },
+        {
+            id: 3,
+            label: '수학'
+        },
+        {
+            id: 4,
+            label: '과학'
+        },
+        {
+            id: 5,
+            label: '사회'
+        },
+        {
+            id: 6,
+            label: '미술'
+        },
+        {
+            id: 7,
+            label: '음악'
+        },
+        {
+            id: 8,
+            label: '체육'
+        },
+        {
+            id: 9,
+            label: '놀이'
+        },
+        {
+            id: 10,
+            label: '기타'
+        },
+        {
+            id: 11,
+            label: '부모'
+        }
+    ]
+
+    const areas = [
+        {
+            id: 1,
+            label: '교구'
+        },
+        {
+            id: 2,
+            label: '교재'
+        },
+        {
+            id: 3,
+            label: '영상'
+        },
+        {
+            id: 4,
+            label: '게임'
+        },
+        {
+            id: 5,
+            label: '블록'
+        },
+        {
+            id: 6,
+            label: '퍼즐'
+        },
+        {
+            id: 7,
+            label: '재료'
+        },
+        {
+            id: 8,
+            label: '기타'
+        },
+    ]
+
+    const obj = {
+        0: <BookshelfInstock />,
+        1: <BookshelfPurchase />,
+        2: <BookshelfSell />
+    }
+
+    const list = (anchor) => (
+
+        <Box
+        sx={{ width: 250 }}
+        role="presentation"
+        onKeyDown={onClick(anchor, false, false)}
+        >
+            <div className='my-2.5'>
+                <div className='mx-3.5'>
+                    <img src='/images/ic_close.png' className='ml-auto' onClick={onClick(anchor, false, false)}/>
+                </div>
+                <div className='mb-7 mx-3.5'>
+                    <h3 className='mb-4 text-base font-semibold'>정렬</h3>
+                    <div className='flex'>
+                        <Global
+                            styles={{
+                                'MuiToggleButtonGroup-root': {
+                                    borderRadius: '6px'
+                                },
+                                '.MuiToggleButton-root.Mui-selected': {
+                                    backgroundColor: '#fff!important',
+                                    color: '#3C81E1',
+                                    borderColor: '#3C81E1',
+                                    borderRadius: '6px'
+                                },
+                            }}
+                        />
+                        <ToggleButtonGroup value={aligns} onChange={handleAligns} aria-label='aligns' className='w-full'>
+                            <ToggleButton value='purchase' aria-label='purchase' className='w-full'>구매순</ToggleButton>
+                            <ToggleButton value='name' aria-label='name' className='w-full'>이름순</ToggleButton>
+                            <ToggleButton value='star' aria-label='star' className='w-full'>별점순</ToggleButton>
+                        </ToggleButtonGroup>
+                    </div>
+                </div>
+                <div className='mb-7 mx-3.5'>
+                    <h3 className='mb-4 text-base font-semibold'>분야</h3>
+                    <div className='flex flex-wrap gap-y-2' style={{columnGap: '7px'}}>
+                        {
+                            fields.map((item, idx) => {
+                                return (
+                                <label className='block relative' key={idx}>
+                                    <input type='checkbox' value={item.id} className='opacity-0 absolute top-0 left-0' onChange={fieldClick}/>
+                                    <span className={`block border border-solid bg-white text-xs 
+                                        ${field[item.id]? 'textBlue4 border-blue4' : 'textGray4 border-gray3'}`} 
+                                        style={{borderRadius: '2px', padding: '5px 8px 6px'}}>{item.label}</span>
+                                </label>
+                                )
+                            })
+                        }                       
+                    </div>
+                </div>
+                <div className='mb-7 mx-3.5'>
+                    <h3 className='mb-4 text-base font-semibold'>영역</h3>
+                    <div className='flex flex-wrap gap-y-2' style={{columnGap: '7px'}}>
+                        {
+                            areas.map((item, idx) => {
+                                return (
+                                <label className='block relative' key={idx}>
+                                    <input type='checkbox' value={item.id} className='opacity-0 absolute top-0 left-0' onChange={areaClick}/>
+                                    <span className={`block border border-solid bg-white text-xs 
+                                        ${area[item.id]? 'textBlue4 border-blue4' : 'textGray4 border-gray3'}`} 
+                                        style={{borderRadius: '2px', padding: '5px 8px 6px'}}>{item.label}</span>
+                                </label>
+                                )
+                            })
+                        }                       
+                    </div>
+                </div>
+                <div className='block absolute bottom-0 mb-6 mx-3.5' style={{width: '90%'}}>
+                    <div className='grid grid-cols-2 gap-x-2 text-center text-sm' style={{height: '44px'}} onClick={clickReturn}>
+                        <div className='flex justify-center rounded-md bg-gray2 items-center'>
+                            <img src='/images/ic_refresh.png' className='w-4 h-4 mr-1'/>다시설정
+                        </div>
+                        <div className='rounded-md bg-blue4 text-white' style={{lineHeight: '44px'}} onClick={onClick(anchor, false, true)}>적용하기</div>
+                    </div>
+                </div>
+            </div>
+        </Box>
+    );
+
+    return (
+        <div>
+            <header className='sticky top-0 left-0 right-0 visible opacity-100 bg-white z-100' style={{marginBottom: '-50px'}}>
+                <div className='my-auto mx-auto py-0 px-4 relative flex items-center w-full bg-white' style={{height: '50px'}}>
+                    <div className='flex-1 flex items-center'>
+                        <div>
+                            <img src='/images/ic_back.png' />
+                        </div>
+                        <div className='my-0 mx-auto text-base font-medium' style={{letterSpacing: '-0.3px'}}>교구장</div>
+                        <div className='flex'>
+                            <img src='/images/filter.png' onClick={onClick('right', true)}/>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <Drawer
+                anchor='right'
+                open={state['right']}
+                onClose={onClick('right', false)}
+            >
+                {list('right')}
+            </Drawer>
+            <main className='mt-12'>
+                <div className='grid grid-cols-3 text-center text-sm textGray4 border-b border-solid border-gray3' style={{height: '40px'}}>
+                    <div className={`font-semibold ${activeTab === 0 ? 'textBlue4 border-b-3 border-solid border-blue4' : ''}`} 
+                        style={{lineHeight: '40px'}} onClick={() => {tabClick(0)}}>구매예정</div>
+                    <div className={`font-semibold ${activeTab === 1 ? 'textBlue4 border-b-3 border-solid border-blue4' : ''}`} 
+                        style={{lineHeight: '40px'}} onClick={() => {tabClick(1)}}>보유중</div>
+                    <div className={`font-semibold ${activeTab === 2 ? 'textBlue4 border-b-3 border-solid border-blue4' : ''}`} 
+                        style={{lineHeight: '40px'}} onClick={() => {tabClick(2)}}>판매완료</div>
+                </div>
+                <section>
+                {
+                    obj[activeTab]
+                }
+                </section>
+            </main>
+            <Link href='/addtool'>
+                <div className='fixed bottom-0 right-0 z-100'>
+                    <img src='/images/ic_float.png'/>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default EduTool;
