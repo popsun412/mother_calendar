1be678d9a5f0ee4a7cd22d57b9a86bbe1df73983
chano <chkim@entcrowd.com>
2022-05-17 11:45:30 +0900

    Update exmap_list.jsx

--- components/exmap/exmap_list.jsx
+++ components/exmap/exmap_list.jsx
@@ -21,7 +21,7 @@ const ExMapList = (props) => {
     const [open, setOpen] = useState(false);
     const [data, setData] = useState([]);
 
-    const getData = async() => {
+    const getData = async () => {
         const res = await axios('http://localhost:4000/map/list', {
             method: 'GET'
         })
@@ -31,7 +31,7 @@ const ExMapList = (props) => {
     useEffect(() => {
         getData();
         const dataNew = [];
-        for(let i=0; i<markers.length; i++) {
+        for (let i = 0; i < markers.length; i++) {
             console.log(markers[i].getPosition())
             data.map((item, idx) => {
                 item.lat == markers[i].getPosition().La && item.lng == markers[i].getPosition().Ma ?
@@ -49,13 +49,13 @@ const ExMapList = (props) => {
 
     const getRate = (rate) => {
         let result = [];
-        
-        for(let i=1; i<=parseInt(rate); i++) {
-            result.push(<img src='/images/ic_star_color.png'/>)
+
+        for (let i = 1; i <= parseInt(rate); i++) {
+            result.push(<img src='/images/ic_star_color.png' />)
         }
 
-        for(let i=1; i<=5-parseInt(rate); i++) {
-            result.push(<img src='/images/ic_star_grey.png'/>)
+        for (let i = 1; i <= 5 - parseInt(rate); i++) {
+            result.push(<img src='/images/ic_star_grey.png' />)
         }
 
         return result;
@@ -84,42 +84,42 @@ const ExMapList = (props) => {
                     keepMounted: true,
                 }}
             >
-            <StyledBox
-                sx={{
-                    position: 'absolute',
-                    top: '-150px',
-                    borderTopLeftRadius: 8,
-                    borderTopRightRadius: 8,
-                    visibility: 'visible',
-                    right: 0,
-                    left: 0,
-                }}
+                <StyledBox
+                    sx={{
+                        position: 'absolute',
+                        top: '-150px',
+                        borderTopLeftRadius: 8,
+                        borderTopRightRadius: 8,
+                        visibility: 'visible',
+                        right: 0,
+                        left: 0,
+                    }}
                 >
-                <div>
-                {
-                    data.map((item, idx) => {
-                        return (
-                            <div className='mx-5 my-4 flex' key={idx}>
-                                <div className='mr-4'>
-                                    <img src='/images/place1.png'/>
-                                </div>
-                                <div>
-                                    <div className='mb-1' style={{fontSize: '15px'}}>{item.title}</div>
-                                    <div className='mb-1 textGray3' style={{fontSize: '13px'}}>{moment(item.date).format('YYYY.MM.DD')} 방문</div>
-                                    <div className='flex'>
-                                        { getRate(item.rate) }
+                    <div>
+                        {
+                            data.map((item, idx) => {
+                                return (
+                                    <div className='mx-5 my-4 flex' key={idx}>
+                                        <div className='mr-4'>
+                                            <img src='/images/place1.png' />
+                                        </div>
+                                        <div>
+                                            <div className='mb-1' style={{ fontSize: '15px' }}>{item.title}</div>
+                                            <div className='mb-1 textGray3' style={{ fontSize: '13px' }}>{moment(item.date).format('YYYY.MM.DD')} 방문</div>
+                                            <div className='flex'>
+                                                {getRate(item.rate)}
+                                            </div>
+                                            <div className='flex'>
+                                                <span className='mr-1 text-xs rounded py-1 px-1.5' style={{ backgroundColor: '#f0f5f8' }}>{item.tag1}</span>
+                                                <span className='text-xs rounded py-1 px-1.5' style={{ backgroundColor: '#f0f5f8' }}>{item.tag2}</span>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div className='flex'>
-                                        <span className='mr-1 text-xs rounded py-1 px-1.5' style={{backgroundColor: '#f0f5f8'}}>{item.tag1}</span>
-                                        <span className='text-xs rounded py-1 px-1.5' style={{backgroundColor: '#f0f5f8'}}>{item.tag2}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-                </div>
-            </StyledBox>
+                                )
+                            })
+                        }
+                    </div>
+                </StyledBox>
             </SwipeableDrawer>
         </Root>
     )
