b50bcf5b5c184e068462b5a58f4635e311685de2
hannaclairekim <hannaclairekim@gmail.com>
2022-05-19 15:35:58 +0900

    장소 상세 수정

---
+++ components/place/place_header.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
+const PlaceHeader = () => {
+
+    const [scrollPosition, setScrollPosition] = useState(0);
+    
+    const updateScroll = () => {
+        setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+    }
+
+    useEffect(()=>{
+        window.addEventListener('scroll', updateScroll);
+    }, []);
+
+    return (
+        <>
+            <header className='sticky top-0 left-0 right-0 opacity-100 visible z-100' style={{ marginBottom: '-50px'}}>
+                <div className={`mx-auto my-0 border-box relative flex items-center py-4 w-full 
+                        ${scrollPosition > 60 ? 'bg-white border-b border-solid border-gray3' : ''}`} style={{height: '50px'}}>
+                {
+                    scrollPosition > 60 ?
+                        <div className='flex mx-5 w-full'>
+                            <div>
+                                <img src='/images/ic_back.png' onClick={() => {window.history.back()}}/>
+                            </div>
+                            <div className='text-center flex-1'>
+                                <span>🏕 애니메이션 센터</span>
+                            </div>
+                            <img src='/images/ic_back.png' className='hidden'/>
+                        </div> : <img src='/images/ic_banner_aos.png' />
+                }
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default PlaceHeader;
