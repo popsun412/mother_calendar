7f0ffd559bbf59d3f5a6a36c03bfc3beef428c82
hannaclairekim <hannaclairekim@gmail.com>
2022-05-23 09:31:37 +0900

    인기 교육템 수정

--- components/home/home_item.jsx
+++ components/home/home_item.jsx
@@ -18,6 +18,10 @@ const HomeItem = (props) => {
         getData();
     }, [])
 
+    const addBookmark = () => {
+        
+    }
+
     return (
         <section style={{margin: '0 20px'}}>
             <h3 className='text-xl font-semibold mb-5' style={{letterSpacing: '-0.4px'}}>인기 교육템</h3>
@@ -29,23 +33,24 @@ const HomeItem = (props) => {
                     data.map((item, idx) => {
                         return (
                             <SwiperSlide key={idx}>
-                                <Link href={{
-                                    pathname: '/item',
-                                    query: { itemUid: item.commonItemUid }
-                                }}>
-                                    <div className='w-24'>
-                                        <div className='block relative'>
-                                            <span className='absolute block top-0 py-1 px-2 text-xs text-white bg-blue3 rounded-tl-md rounded-br-md'>{idx+1}위</span>
-                                            <img src={item.image} className='rounded-md' style={{width: '94px', height: '94px'}}/>
-                                            <img src={`/images/ic_${item.bookmark? 'bookmarked.png' : 'bookmark.png'}`}  className='block absolute bottom-0 right-0 mr-2 mb-1.5' />
-                                        </div>
-                                        <div className='text-xs leading-tight mt-1.5' style={{letterSpacing: '-0.26px'}}>{item.name}</div>
-                                        <div className='flex mt-1'>
-                                            <span className='py-0.7 px-1.5 mr-1 rounded text-center text-xs textGray3' style={{backgroundColor: '#f0f5f8'}}>{item.field}</span>
-                                            <span className='py-0.7 px-1.5 mr-1 rounded text-center text-xs textGray3' style={{backgroundColor: '#f0f5f8'}}>{item.subject}</span>
-                                        </div>
+                                <div className='w-24'>
+                                    <div className='block relative'>
+                                        <span className='absolute block top-0 py-1 px-2 text-xs text-white bg-blue3 rounded-tl-md rounded-br-md'>{idx+1}위</span>
+                                        <img src={item.image} className='rounded-md' style={{width: '94px', height: '94px'}}/>
+                                        <img src={`/images/ic_${item.bookmark? 'bookmarked.png' : 'bookmark.png'}`}  
+                                            className='block absolute bottom-0 right-0 mr-2 mb-1.5' onClick={addBookmark}/>
                                     </div>
-                                </Link>
+                                    <Link href={{
+                                        pathname: '/item',
+                                        query: { itemUid: item.commonItemUid }
+                                    }}>
+                                        <div className='text-xs leading-tight mt-1.5' style={{letterSpacing: '-0.26px'}}>{item.name}</div>
+                                    </Link>
+                                    <div className='flex mt-1'>
+                                        <span className='py-0.7 px-1.5 mr-1 rounded text-center text-xs textGray3' style={{backgroundColor: '#f0f5f8'}}>{item.field}</span>
+                                        <span className='py-0.7 px-1.5 mr-1 rounded text-center text-xs textGray3' style={{backgroundColor: '#f0f5f8'}}>{item.subject}</span>
+                                    </div>
+                                </div>
                             </SwiperSlide>
                         )
                     })
