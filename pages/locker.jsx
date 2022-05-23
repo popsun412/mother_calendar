d8751533a5ec5a87a88a33bc256b6153a60317ed
hannaclairekim <hannaclairekim@gmail.com>
2022-05-23 15:03:15 +0900

    보관함 메인 Link 추가

--- pages/locker.jsx
+++ pages/locker.jsx
@@ -1,4 +1,5 @@
 import { Box, Button, Drawer } from '@material-ui/core';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 
 const Locker = () => {
@@ -26,35 +27,51 @@ const Locker = () => {
                 <div className='mt-8 grid grid-rows-4'>
                     <div className='flex justify-center items-center' 
                         style={{width: '200px', height: '140px', borderRadius: '15px', backgroundColor: '#f8f6f5', marginBottom: '18px'}}>
-                        <div className='text-center'>
-                            <div className='flex justify-center'><img src='/images/ic_book@2x.png' style={{width: '44px', height: '44px'}}/></div>
-                            <div className='mt-2 text-lg font-semibold'>책장</div>
-                            <div className='text-base'>5개</div>
-                        </div>
+                        <Link href={{
+                            pathname: '/bookshelf'
+                        }}>
+                            <div className='text-center'>
+                                <div className='flex justify-center'><img src='/images/ic_book@2x.png' style={{width: '44px', height: '44px'}}/></div>
+                                <div className='mt-2 text-lg font-semibold'>책장</div>
+                                <div className='text-base'>5개</div>
+                            </div>
+                        </Link>
                     </div>
                     <div className='flex justify-center items-center' 
                         style={{width: '200px', height: '140px', borderRadius: '15px', backgroundColor: '#f8f6f5', marginBottom: '18px'}}>
-                        <div className='text-center'>
-                            <div className='flex justify-center'><img src='/images/ic_teaching_aid@2x.png' style={{width: '44px', height: '44px'}}/></div>
-                            <div className='mt-2 text-lg font-semibold'>책장</div>
-                            <div className='text-base'>5개</div>
-                        </div>
+                        <Link href={{
+                            pathname: '/edutool'
+                        }}>
+                            <div className='text-center'>
+                                <div className='flex justify-center'><img src='/images/ic_teaching_aid@2x.png' style={{width: '44px', height: '44px'}}/></div>
+                                <div className='mt-2 text-lg font-semibold'>교구장</div>
+                                <div className='text-base'>5개</div>
+                            </div>
+                        </Link>
                     </div>
                     <div className='flex justify-center items-center' 
                         style={{width: '200px', height: '140px', borderRadius: '15px', backgroundColor: '#f8f6f5', marginBottom: '18px'}}>
-                        <div className='text-center'>
-                            <div className='flex justify-center'><img src='/images/ic_academy@2x.png' style={{width: '44px', height: '44px'}}/></div>
-                            <div className='mt-2 text-lg font-semibold'>책장</div>
-                            <div className='text-base'>5개</div>
-                        </div>
+                        <Link href={{
+                            pathname: '/instimap'
+                        }}>
+                            <div className='text-center'>
+                                <div className='flex justify-center'><img src='/images/ic_academy@2x.png' style={{width: '44px', height: '44px'}}/></div>
+                                <div className='mt-2 text-lg font-semibold'>학원지도</div>
+                                <div className='text-base'>5개</div>
+                            </div>
+                        </Link>
                     </div> 
                     <div className='flex justify-center items-center' 
                         style={{width: '200px', height: '140px', borderRadius: '15px', backgroundColor: '#f8f6f5', marginBottom: '18px'}}>
-                        <div className='text-center'>
-                            <div className='flex justify-center'><img src='/images/ic_field_trip@2x.png' style={{width: '44px', height: '44px'}}/></div>
-                            <div className='mt-2 text-lg font-semibold'>책장</div>
-                            <div className='text-base'>5개</div>
-                        </div>
+                        <Link href={{
+                            pathname: '/exmap'
+                        }}>
+                            <div className='text-center'>
+                                <div className='flex justify-center'><img src='/images/ic_field_trip@2x.png' style={{width: '44px', height: '44px'}}/></div>
+                                <div className='mt-2 text-lg font-semibold'>체험지도</div>
+                                <div className='text-base'>5개</div>
+                            </div>
+                        </Link>
                     </div> 
                 </div>
             </div>
