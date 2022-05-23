052469c4736c07faa828e359004d2efe2d877400
hannaclairekim <hannaclairekim@gmail.com>
2022-05-23 14:24:18 +0900

    plan 컴포넌트 주석처리

--- pages/plan/index.jsx
+++ pages/plan/index.jsx
@@ -1,10 +1,10 @@
-import PlanCertify from "../../components/plan/plan_certify";
-import PlanCompletion from "../../components/plan/plan_completion";
-import PlaneDetail from "../../components/plan/plan_detail";
-import PlanDetailModify from "../../components/plan/plan_detail_ modify";
-import PlanItemAdd from "../../components/plan/plan_item_add";
-import PlanRegistartion from "../../components/plan/plan_registration";
-import PlanExperience from "../../components/plan/plan_experience";
+// import PlanCertify from "../../components/plan/plan_certify";
+// import PlanCompletion from "../../components/plan/plan_completion";
+// import PlaneDetail from "../../components/plan/plan_detail";
+// import PlanDetailModify from "../../components/plan/plan_detail_ modify";
+// import PlanItemAdd from "../../components/plan/plan_item_add";
+// import PlanRegistartion from "../../components/plan/plan_registration";
+// import PlanExperience from "../../components/plan/plan_experience";
 
 
 export default function Plan() {
